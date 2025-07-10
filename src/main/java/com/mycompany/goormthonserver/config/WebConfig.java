aa4b5b7fc61baef75037e3e6b1dd5935a9089aa0
yunrry <yunryy329@gmail.com>
2025-07-10 18:57:44 +0900

    webconfig

---
+++ src/main/java/com/mycompany/goormthonserver/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.mycompany.goormthonserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://dormung.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // 필요 시 쿠키 사용 허용
+    }
+}
